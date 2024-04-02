07136c00732c0d4c961ade0fdae4d0c6ebc45ccc
carloshenriquehannas <carloshenriquehannas@gmail.com>
2024-04-01 21:35:20 -0300

    Tratamento de viagens de ida e volta

--- Atividades/ListaProlog.pl
+++ Atividades/ListaProlog.pl
@@ -1,4 +1,4 @@
-% Exercicio 4
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Exercicio 4 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %Fatos: ha viagem de onibus partindo de (origem, destino)
 onibus(araraquara, saocarlos).
@@ -10,9 +10,14 @@ onibus(marilia, bauru).
 onibus(ribeirao, araraquara).
 onibus(saocarlos, bauru).
 
-%Regras: viagem entre uma origem e um destino, com 0 ou mais cidades intermediarias
-viagem_entre(Origem, Destino):-
-    onibus(Origem, Destino).                                %Caso em que ha viagem direta
-viagem_entre(Origem, Destino):-
-    onibus(Origem, Parada), viagem_entre(Parada, Destino).  %Caso em que ha cidade(s) intermediaria(s)
-    
+%Regras: viagem entre uma origem e um destino, com ou sem paradas
+viagem_entre(Origem, Destino):-                                 %Caso em que ha viagem direta
+    onibus(Origem, Destino), !;                                 %Viagem de ida
+    onibus(Destino, Origem).                                    %Viagem de volta
+
+viagem_entre(Origem, Destino):-                                 %Caso em que ha cidade(s) intermediaria(s)
+    onibus(Origem, Parada), viagem_entre(Parada, Destino), !;   %Viagem de ida com paradas 
+    onibus(Destino, Parada), viagem_entre(Parada, Origem).      %Viagem de volta com paradas
+
+%Utilizou-se o corte para que nao houvesse resposta repetida quando ha viagem de ida e volta
+
